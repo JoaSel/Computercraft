9f653f1f7a0c705808d979bacfb5dcb1725f6af1
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 11:07:31 +0200

    WIP

--- install.lua
+++ install.lua
@@ -49,10 +49,14 @@ print("Installing SquidDev's git clone...")
 shell.run("wget", "https://gist.githubusercontent.com/SquidDev/e0f82765bfdefd48b0b15a5c06c0603b/raw/clone.min.lua", "clone.lua")
 
 print("Creating startup file...")
-local repoBakName = repoName .. "Bak"
-shell.execute("rm", repoBakName)
-shell.execute("mv", repoName, repoBakName)
-if(not shell.execute("clone.lua", gitUrl .. repoName) and fs.exists(repoBakName)) then
-    shell.execute("mv", repoBakName, repoName)
-end
-shell.execute(repoName .. "/" .. scriptName)
+local startupFile = fs.open("startup", "w+")
+
+startupFile.write(string.format("local repoBakName = %s"), repoName .. "Bak")
+
+-- local repoBakName = repoName .. "Bak"
+-- shell.execute("rm", repoBakName)
+-- shell.execute("mv", repoName, repoBakName)
+-- if(not shell.execute("clone.lua", gitUrl .. repoName) and fs.exists(repoBakName)) then
+--     shell.execute("mv", repoBakName, repoName)
+-- end
+-- shell.execute(repoName .. "/" .. scriptName)
