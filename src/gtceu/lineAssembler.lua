6af79485afb63e17b8764bb2c0548fa638d45473
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 17:16:58 +0200

    WIP

--- src/gtceu/lineAssembler.lua
+++ src/gtceu/lineAssembler.lua
@@ -12,7 +12,7 @@ local output = peripheral.wrap("entangled:tile_21")
 local dataAccessHatch = peripheral.find("gtceu:data_access_hatch")
 
 local inputBuses = { peripheral.find("gtceu:ulv_input_bus") }
-local inputHatches = { peripheral.find("gtceu:ulv_input_hatch") }
+local inputHatches = { peripheral.find("gtceu:lv_input_hatch") }
 
 local outputBuses = { peripheral.find("gtceu:ulv_output_bus") }
 local outputHatches = { peripheral.find("gtceu:ulv_output_hatch") }
