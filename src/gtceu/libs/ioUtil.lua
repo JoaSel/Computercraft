ef0762e96e433b07417f840893c8da0ee0282c50
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 19:02:31 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -5,29 +5,13 @@ local pWrapper = require("peripheralWrapper")
 local tiers = {"ulv", "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv"}
 
 local function findInputBuses()
-    local all = {}
+    local ret = {}
 
     for _, tier in pairs(tiers) do
-        table.insert(all, { pWrapper.find("gtceu:" .. tier .. "_input_bus") })
-    end
+        local perips = { pWrapper.find("gtceu:" .. tier .. "_input_bus") }
 
-    -- local all = {
-    --     { pWrapper.find("gtceu:ulv_input_bus") },
-    --     { pWrapper.find("gtceu:lv_input_bus") },
-    --     { pWrapper.find("gtceu:mv_input_bus") },
-    --     { pWrapper.find("gtceu:hv_input_bus") },
-    --     { pWrapper.find("gtceu:ev_input_bus") },
-    --     { pWrapper.find("gtceu:iv_input_bus") },
-    --     { pWrapper.find("gtceu:luv_input_bus") },
-    --     { pWrapper.find("gtceu:zpm_input_bus") },
-    --     { pWrapper.find("gtceu:uv_input_bus") },
-    --     { pWrapper.find("gtceu:uhv_input_bus") },
-    -- }
-
-    local ret = {}
-    for _, perips in pairs(all) do
-        for _, p in pairs(perips) do
-            table.insert(ret, p)
+        for _, perip in pairs(perips) do
+            table.insert(ret, perip)
         end
     end
 
