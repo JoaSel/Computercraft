1b2eb6ec2c456f579020838e3fea5c485e0373f5
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 19:38:53 +0200

    WIP

--- src/gtceu/lineAssembler.lua
+++ src/gtceu/lineAssembler.lua
@@ -35,6 +35,7 @@ local function importItems()
     local busSize = currentBus.size()
     for fromSlot, item in pairs(inputItems) do
         print("Importing " .. item.name .. " to " .. currentBus.name)
+        print(busSize)
         input.pushItems(currentBus.name, fromSlot, 64)
         sentToThisBus = sentToThisBus + 1
 
