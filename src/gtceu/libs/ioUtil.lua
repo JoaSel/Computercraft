30e26a8ad1e5955f5381f3fca611fb0b9bf9924f
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 19:03:20 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -22,4 +22,22 @@ local function findInputBuses()
     return unpack(ret)
 end
 
-return { findInputBuses = findInputBuses }
+local function findOutputBuses()
+    local ret = {}
+
+    for _, tier in pairs(tiers) do
+        local perips = { pWrapper.find("gtceu:" .. tier .. "_output_bus") }
+
+        for _, perip in pairs(perips) do
+            table.insert(ret, perip)
+        end
+    end
+
+    table.sort(ret, function (a, b)
+        return a.name < b.name
+    end)
+
+    return unpack(ret)
+end
+
+return { findInputBuses = findInputBuses, findOutputBuses = findOutputBuses }
