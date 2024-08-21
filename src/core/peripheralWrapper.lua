e6b9f503ee830460202067b8743fe974543b0f0d
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 18:30:00 +0200

    WIP

--- src/core/peripheralWrapper.lua
+++ src/core/peripheralWrapper.lua
@@ -2,18 +2,18 @@ local function find(peripheralType)
     local peripherals = { peripheral.find(peripheralType) }
 
     for _, p in pairs(peripherals) do
-        peripheral.name = peripheral.getName(p)
+        p.name = peripheral.getName(p)
     end
 
     return unpack(peripherals)
 end
 
 local function wrap(peripheralName)
-    local peripheral = peripheral.wrap(peripheralName)
+    local p = peripheral.wrap(peripheralName)
 
-    peripheral.name = peripheral.getName(p)
+    p.name = peripheral.getName(p)
 
-    return peripheral
+    return p
 end
 
 return { find = find, wrap = wrap }
