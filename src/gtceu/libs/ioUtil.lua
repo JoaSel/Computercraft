e255ae9ebff7e165d07b2c2b0c2e49cc43e832c7
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 19:05:36 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -4,11 +4,11 @@ local pWrapper = require("peripheralWrapper")
 
 local tiers = {"ulv", "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv"}
 
-local function findInputBuses()
+local function find(postfix)
     local ret = {}
 
     for _, tier in pairs(tiers) do
-        local perips = { pWrapper.find("gtceu:" .. tier .. "_input_bus") }
+        local perips = { pWrapper.find("gtceu:" .. tier .. postfix) }
 
         for _, perip in pairs(perips) do
             table.insert(ret, perip)
@@ -22,22 +22,12 @@ local function findInputBuses()
     return unpack(ret)
 end
 
+local function findInputBuses()
+    return find("_input_bus")
+end
+
 local function findOutputBuses()
-    local ret = {}
-
-    for _, tier in pairs(tiers) do
-        local perips = { pWrapper.find("gtceu:" .. tier .. "_output_bus") }
-
-        for _, perip in pairs(perips) do
-            table.insert(ret, perip)
-        end
-    end
-
-    table.sort(ret, function (a, b)
-        return a.name < b.name
-    end)
-
-    return unpack(ret)
+    return find("_output_bus")
 end
 
 return { findInputBuses = findInputBuses, findOutputBuses = findOutputBuses }
