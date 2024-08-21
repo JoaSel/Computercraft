68c251977b5c03a20a2143ae680cdbd8f050a354
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 18:51:27 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -2,15 +2,15 @@ package.path = package.path .. ";../../core/?.lua"
 
 local dump = require("dump")
 local pWrapper = require("peripheralWrapper")
-
-
 local function findInputBuses()
-    local ulv = pWrapper.find("gtceu:ulv_input_bus")
-    local lv = pWrapper.find("gtceu:lv_input_bus")
+    local all = {
+        { pWrapper.find("gtceu:ulv_input_bus") },
+        { pWrapper.find("gtceu:lv_input_bus") }
+    }
 
-    dump.printDump(lv)
+    dump.printDump(all)
 
-    return ulv
+    return nil
 end
 
 return { findInputBuses = findInputBuses }
