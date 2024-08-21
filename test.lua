6e9e0b37586f0f6d75b8c10cc1ddba097846f6de
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 13:25:37 +0200

    WIP

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 --wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua test.lua
 
-local debug = require("src.Debug.basics")
+local debug = require("src.debug.basics")
 
-debug.PrintDump({name = "test"})
+debug.printDump({name = "test"})
