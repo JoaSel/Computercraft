2d29bb1db8a28d8af5c2c76853026fd8d758f513
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 18:56:28 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -5,12 +5,29 @@ local pWrapper = require("peripheralWrapper")
 local function findInputBuses()
     local all = {
         { pWrapper.find("gtceu:ulv_input_bus") },
-        { pWrapper.find("gtceu:lv_input_bus") }
+        { pWrapper.find("gtceu:lv_input_bus") },
+        { pWrapper.find("gtceu:mv_input_bus") },
+        { pWrapper.find("gtceu:hv_input_bus") },
+        { pWrapper.find("gtceu:ev_input_bus") },
+        { pWrapper.find("gtceu:iv_input_bus") },
+        { pWrapper.find("gtceu:luv_input_bus") },
+        { pWrapper.find("gtceu:zpm_input_bus") },
+        { pWrapper.find("gtceu:uv_input_bus") },
+        { pWrapper.find("gtceu:uhv_input_bus") },
     }
 
-    dump.printDump(all)
+    local ret = {}
+    for _, perips in pairs(all) do
+        for _, p in pairs(perips) do
+            table.insert(ret, p)
+        end
+    end
 
-    return nil
+    table.sort(ret, function (a, b)
+        return a.name < b.name
+    end)
+
+    return unpack(ret)
 end
 
 return { findInputBuses = findInputBuses }
