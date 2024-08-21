a01117f3d47d743e4f503407ebbe102a2c73b9dd
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 10:07:07 +0200

    .

--- install.lua
+++ install.lua
@@ -1,4 +1,16 @@
 local args = { ... }
 
-print(#args + 5)
+--Create startup file that does
+    --mv old repo file
+    --get a github repo
+    --*if it failed mv back
+
+if(#args ~= 1) then
+    error("Usage: ....")
+end
+
+local address = args[1]
+
+
+print(address)
 
