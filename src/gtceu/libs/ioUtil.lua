f7e4e1342a33257f88065faf185ac2616a825016
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 19:01:13 +0200

    WIP

--- src/gtceu/libs/ioUtil.lua
+++ src/gtceu/libs/ioUtil.lua
@@ -1,20 +1,28 @@
 package.path = package.path .. ";../../core/?.lua"
 
-local dump = require("dump")
 local pWrapper = require("peripheralWrapper")
+
+local tiers = {"ulv", "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv"}
+
 local function findInputBuses()
-    local all = {
-        { pWrapper.find("gtceu:ulv_input_bus") },
-        { pWrapper.find("gtceu:lv_input_bus") },
-        { pWrapper.find("gtceu:mv_input_bus") },
-        { pWrapper.find("gtceu:hv_input_bus") },
-        { pWrapper.find("gtceu:ev_input_bus") },
-        { pWrapper.find("gtceu:iv_input_bus") },
-        { pWrapper.find("gtceu:luv_input_bus") },
-        { pWrapper.find("gtceu:zpm_input_bus") },
-        { pWrapper.find("gtceu:uv_input_bus") },
-        { pWrapper.find("gtceu:uhv_input_bus") },
-    }
+    local all = {}
+
+    for _, tier in pairs(tiers) do
+        table.insert(all, { pWrapper.find("gtceu:" .. tier .. "_input_bus") })
+    end
+
+    -- local all = {
+    --     { pWrapper.find("gtceu:ulv_input_bus") },
+    --     { pWrapper.find("gtceu:lv_input_bus") },
+    --     { pWrapper.find("gtceu:mv_input_bus") },
+    --     { pWrapper.find("gtceu:hv_input_bus") },
+    --     { pWrapper.find("gtceu:ev_input_bus") },
+    --     { pWrapper.find("gtceu:iv_input_bus") },
+    --     { pWrapper.find("gtceu:luv_input_bus") },
+    --     { pWrapper.find("gtceu:zpm_input_bus") },
+    --     { pWrapper.find("gtceu:uv_input_bus") },
+    --     { pWrapper.find("gtceu:uhv_input_bus") },
+    -- }
 
     local ret = {}
     for _, perips in pairs(all) do
