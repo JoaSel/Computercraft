c1ad602001ce8aa2cb76f0fa92f57406d4416a94
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 18:22:29 +0200

    WIP

--- src/core/peripheralWrapper.lua
+++ src/core/peripheralWrapper.lua
@@ -1,9 +1,7 @@
 
 
 local function find(peripheralType)
-    local peripherals = peripheral.find(peripheralType)
-
-    print(peripherals)
+    local peripherals = { peripheral.find(peripheralType) }
 
     for _, p in pairs(peripherals) do
         peripheral.name = peripheral.getName(p)
