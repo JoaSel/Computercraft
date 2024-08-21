dbb9a3357ad0ca9ec8b55e7fdc1365402cdad5d3
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 16:39:55 +0200

    WIP

--- src/gtceu/lineAssembler.lua
+++ src/gtceu/lineAssembler.lua
@@ -1,8 +1,9 @@
----@diagnostic disable: param-type-mismatch
 --wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua src/gtceu/lineAssembler.lua
 
 package.path = package.path .. ";../debug/?.lua"
 
 local debug = require("debug")
 
+print(debug)
 debug.printDump({name = "test"})
+
