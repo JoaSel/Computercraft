66feccd7b471eccfc1eeb83c56abcda894671c32
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 14:19:59 +0200

    WIP

---
+++ src/ae2/meDefragmenter.lua
@@ -0,0 +1,175 @@
+--wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua src/ae2/meDefragmenter.lua
+
+package.path = package.path .. ";../core/?.lua"
+package.path = package.path .. ";../debug/?.lua"
+
+local debug = require("debug")
+local jGui = require("jGui")
+
+local monitor = peripheral.find("monitor")
+local mWidth, mHeight = monitor.getSize()
+monitor.clear()
+
+local bulkStorages = { peripheral.find("dankstorage:dank_tile") }
+local bulkStorageI = 1
+
+local nbtStorages = { peripheral.find("entangled:tile") }
+local nbtStorageI = 1
+local nbtStorageLimit = 1024
+
+local unstackableItems = {
+	["minecraft:bundle"] = true
+}
+
+local function indexItem(item, pName, collection)
+	if (item.nbt == nil) then
+		collection[item.name] = pName
+	else
+		collection[item.name .. item.nbt] = pName
+	end
+end
+
+local function indexItemLoc(item, peripheral, slot, collection)
+	local index = nil
+
+	if (item.nbt == nil) then
+		index = item.name
+	else
+		index = item.name .. item.nbt
+	end
+
+	if (not collection[index]) then
+		collection[index] = {}
+	end
+	table.insert(collection[index], { count = item.count, peripheral = peripheral, slot = slot })
+end
+
+local function tryGet(item, collection)
+	if (item.nbt == nil) then
+		return collection[item.name]
+	else
+		return collection[item.name .. item.nbt]
+	end
+end
+
+local function moveItems(fromPeripheral, toLoc, fromSlot, toMoveCount)
+	local ret = 0
+	repeat
+		local moved = fromPeripheral.pushItems(toLoc, fromSlot)
+		ret = ret + moved
+	until (ret >= toMoveCount or moved == 0)
+	return ret
+end
+
+local function defragmentStorages(storages)
+	local allItems = {}
+	local occupied = 0
+	local total = 0
+
+	for i = 1, #storages do
+		local current = storages[i]
+		local currentName = peripheral.getName(current)
+
+		local currentItems = current.list()
+
+		occupied = occupied + #currentItems
+		total = total + current.size()
+
+		for slot, item in pairs(currentItems) do
+			if (not unstackableItems[item.name]) then
+				local existingLoc = tryGet(item, allItems)
+				if (existingLoc ~= nil) then
+					print(item.count .. " " .. item.name .. " " .. currentName .. " => " .. existingLoc)
+					moveItems(current, existingLoc, slot, item.count)
+				else
+					indexItem(item, currentName, allItems)
+				end
+			end
+		end
+	end
+	return occupied, total
+end
+
+local function moveToNbt()
+	for i = 1, #bulkStorages do
+		local current = bulkStorages[i]
+		local currentName = peripheral.getName(current)
+
+		for slot, item in pairs(current.list()) do
+			if (item.count < nbtStorageLimit) then
+				print("Moving " .. item.name .. " to NBT Storage")
+				moveItems(current, peripheral.getName(nbtStorages[nbtStorageI]), slot, item.count)
+				nbtStorageI = (nbtStorageI % #nbtStorages) + 1
+			end
+		end
+	end
+end
+
+local function moveToBulk()
+	local allItems = {}
+	for i = 1, #nbtStorages do
+		local current = nbtStorages[i]
+		local currentName = peripheral.getName(current)
+
+		for slot, item in pairs(current.list()) do
+			indexItemLoc(item, current, slot, allItems)
+		end
+	end
+
+	for itemName, locations in pairs(allItems) do
+		local sum = 0
+		for i = 1, #locations do
+			sum = sum + locations[i].count
+		end
+		if (sum > 1024) then
+			for i = 1, #locations do
+				print("Moving " .. itemName .. " to Bulk Storage")
+				moveItems(locations[i].peripheral, peripheral.getName(bulkStorages[bulkStorageI]), locations[i].slot,
+					locations[i].count)
+			end
+			bulkStorageI = (bulkStorageI % #bulkStorages) + 1
+		end
+	end
+end
+
+local function setTextLine(text, line)
+	monitor.setCursorPos(1, line)
+	monitor.clearLine()
+	jGui.writeCenter(text)
+end
+
+jGui.setMonitor(monitor)
+
+setTextLine("Bulk Storage", 1)
+setTextLine("NBT Storage", 6)
+
+local created = false
+while (true) do
+	setTextLine("Defragmenting", 11)
+	setTextLine("Bulk Storage", 12)
+	local bulkOcc, bulkTot = defragmentStorages(bulkStorages)
+
+	setTextLine("Moving items to", 11)
+	setTextLine("NBT Storage", 12)
+	moveToNbt()
+
+	setTextLine("Defragmenting", 11)
+	setTextLine("NBT Storage", 12)
+	local nbtOcc, nbtTot = defragmentStorages(nbtStorages)
+
+	setTextLine("Moving items to", 11)
+	setTextLine("Bulk Storage", 12)
+	moveToBulk()
+
+	if (not created) then
+		jGui.createSlider("bulkSlider", bulkTot, 2, 2, mWidth - 2, 3, colors.red, colors.lime, jGui.Numbers)
+		jGui.createSlider("nbtSlider", nbtTot, 2, 7, mWidth - 2, 3, colors.red, colors.lime, jGui.Numbers)
+		created = true
+	end
+
+	jGui.updateSlider("bulkSlider", bulkOcc)
+	jGui.updateSlider("nbtSlider", nbtOcc)
+	jGui.draw()
+
+	os.sleep(5)
+end
---
+++ src/core/jGui.lua
@@ -0,0 +1,151 @@
+---@diagnostic disable: need-check-nil
+
+local sliders = {}
+local monitor = nil
+local mWidth = nil
+local mHeight = nil
+
+local None = 0
+local Percent = 1
+local Numbers = 2
+
+local function setMonitor(peripheral)
+	if not (peripheral.isColor()) then
+		error("Monitor Doesn't Support Colors")
+	end
+	monitor = peripheral
+	mWidth, _ = peripheral.getSize()
+end
+
+local function writeCenter(text)
+	local _, y = monitor.getCursorPos()
+	monitor.setCursorPos(math.floor((mWidth / 2) - (#text / 2)) + 1, y)
+	monitor.write(text)
+end
+
+local function createSlider(name, maxValue, x, y, length, height, sliderColor, barColor, infoType)
+	if (sliders[name]) then
+		error(name .. " already exist!")
+	end
+
+	if sliderColor == nil then
+		sliderColor = colors.gray
+	end
+	if barColor == nil then
+		barColor = colors.white
+	end
+	if infoType == nil then
+		infoType = 1
+	end
+
+	-- fills in values
+	sliders[name] = {}
+	sliders[name]["maxValue"] = maxValue
+	sliders[name]["x"] = x
+	sliders[name]["y"] = y
+	sliders[name]["length"] = length
+	sliders[name]["height"] = height
+	sliders[name]["sliderColor"] = sliderColor
+	sliders[name]["barColor"] = barColor
+	sliders[name]["value"] = 0
+	sliders[name]["textColor"] = colors.black
+	sliders[name]["infoType"] = infoType
+end
+
+local function updateSlider(name, value)
+	local slider = sliders[name]
+	if (slider) then
+		if (value > slider.maxValue) then
+			value = slider.maxValue
+		elseif value < 0 then
+			error(name .. ": Value can not be under 0!")
+		end
+		slider.value = value
+	end
+end
+
+local function drawCenterInfo(v, text, percentDraw)
+	local textX = math.floor(v.x + (v.length / 2) - (#text / 2))
+	local textY = math.floor(v.y + v.height - (v.height / 2))
+
+	monitor.setCursorPos(textX, textY)
+	monitor.setTextColor(v.textColor)
+
+	for i = 0, #text do
+		if (textX + i > percentDraw + 2) then
+			monitor.setBackgroundColor(v.barColor)
+		else
+			monitor.setBackgroundColor(v.sliderColor)
+		end
+		monitor.write(string.sub(text, i, i))
+	end
+end
+
+local function drawPercent(v, percentDraw)
+	local text = math.floor(v.value * 100 / v.maxValue) .. "%"
+	drawCenterInfo(v, text, percentDraw)
+end
+
+local function drawNumbers(v, percentDraw)
+	local text = v.value .. " / " .. v.maxValue
+	drawCenterInfo(v, text, percentDraw)
+end
+
+local function draw(name)
+	if monitor == nil then
+		monitor = term
+		if not (monitor.isColor()) then
+			error("Monitor doesn't support Colors")
+		end
+	end
+
+	if not (type(name) == "table") then
+		if not (type(name) == "string") then
+			name = {}
+			for k, v in pairs(sliders) do
+				table.insert(name, k)
+			end
+		else
+			name = { name }
+		end
+	end
+
+	for k, v in pairs(sliders) do
+		for s = 0, #name + 1 do
+			if k == name[s] then
+				local percentDraw = v.length * (v.value / v.maxValue)
+				for yPos = v.y, v.y + v.height - 1 do
+					monitor.setBackgroundColor(v.barColor)
+					monitor.setCursorPos(v.x, yPos)
+					monitor.write(string.rep(" ", v.length))
+
+					monitor.setCursorPos(v.x, yPos)
+					monitor.setBackgroundColor(v.sliderColor)
+					monitor.write(string.rep(" ", percentDraw))
+
+					if (v.infoType == 1) then
+						drawPercent(v, percentDraw)
+					end
+					if (v.infoType == 2) then
+						drawNumbers(v, percentDraw)
+					end
+				end
+			end
+		end
+
+		monitor.setBackgroundColor(colors.black)
+		monitor.setTextColor(colors.white)
+		monitor.setCursorPos(1, 1)
+	end
+end
+
+return {
+	setMonitor = setMonitor,
+	writeCenter = writeCenter,
+	createSlider = createSlider,
+	updateSlider = updateSlider,
+	drawCenterInfo = drawCenterInfo,
+	drawPercent = drawPercent,
+	drawNumbers = drawNumbers,
+	draw = draw
+}
