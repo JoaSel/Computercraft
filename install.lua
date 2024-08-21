4cfd7890e1709777e5d8ee610c861e82d9b8d980
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-21 10:33:25 +0200

    WIP

--- install.lua
+++ install.lua
@@ -12,29 +12,9 @@ end
 local repoName = "https://github.com/JoaSel/Computercraft"
 local scriptName = args[1]
 
-local function getRequestData()
-    local requestDataLocationFile = fs.open("requestDataLocation", "r")
-    local requestDataLocation = requestDataLocationFile.readLine()
-    requestDataLocationFile.close()
-    
-    local socket = http.get(requestDataLocation)
-    local content = socket.readAll()
-    socket.close()
- 
-    if(not content) then
-        error("Could not get request data.")
-    end
-    local parsed = textutils.unserialize(content)
-    if(not parsed) then
-        error("Could not unserialize request data.")
-    end
- 
-    return parsed 
-end
-
 local function updateFile(url, filename)
+    local backupFilename = filename .. ".bak"
     if(fs.exists(filename)) then
-        local backupFilename = filename .. ".bak"
         print("Backing up " .. filename .. " => " .. backupFilename)
         fs.move(filename, backupFilename)
     end
@@ -46,17 +26,14 @@ local function updateFile(url, filename)
     local file = fs.open(filename, "w")
     file.write(content)
     file.close()
-    -- local requestDataLocationFile = fs.open("requestDataLocation", "r")
-    -- local requestDataLocation = requestDataLocationFile.readLine()
-    -- requestDataLocationFile.close()
+
+    if(fs.exists(backupFilename)) then
+        fs.delete(backupFilename)
+    end
 end
 
-local function installGitClone()
-    print("Installing SquidDev's git clone...")
-    updateFile("https://gist.githubusercontent.com/SquidDev/e0f82765bfdefd48b0b15a5c06c0603b/raw/clone.min.lua", "clone.lua")
-end
-
-installGitClone()
+print("Installing SquidDev's git clone...")
+updateFile("https://gist.githubusercontent.com/SquidDev/e0f82765bfdefd48b0b15a5c06c0603b/raw/clone.min.lua", "clone.lua")
 
 print("Installing " .. scriptName .. " from " .. repoName)
 
