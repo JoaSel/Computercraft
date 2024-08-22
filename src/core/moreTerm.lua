1f34eb06afb1267f534ec35552d3c342636a0f6f
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 14:44:30 +0200

    WIP

--- src/core/moreTerm.lua
+++ src/core/moreTerm.lua
@@ -1,6 +1,6 @@
-local function print(text, color)
+local function print(text, textColor)
     local oldColor = term.getTextColor()
-    term.setTextColor(color or colors.pink)
+    term.setTextColor(textColor or colors.pink)
     print(text)
     term.setTextColor(oldColor)
 end
