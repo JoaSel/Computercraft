b1086709738ecc519a9ac174542b922cc072cec3
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 17:28:49 +0200

    WIP

--- test.lua
+++ test.lua
@@ -1,6 +1,8 @@
 --wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua test.lua
 
-local time = require("src.core.time")
+local pWrapper = require("src.core.peripheralWrapper")
 local dump = require("src.core.dump")
 
-time.sleep(1, 2)
+local x = pWrapper.find("blockReader")
+
+dump.easy(x.getBlockData())
