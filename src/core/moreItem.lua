3b41298df13dd4787938c97d7face92380cbf505
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 20:05:24 +0200

    WIP

--- src/core/moreItem.lua
+++ src/core/moreItem.lua
@@ -1,10 +1,3 @@
-local function hasTag(item, tag)
-	if (item.tags == nil) then
-		return false
-	elseif (item.tags[tag] == nil) then
-		return false
-	end
-	return true
-end
 
-return { hasTag = hasTag }
+
+return {  }
