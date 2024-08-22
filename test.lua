029ac06b78050b1b4a61e0bf3b88c4abd4b69289
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 19:33:47 +0200

    WIP

--- test.lua
+++ test.lua
@@ -6,5 +6,5 @@ local dump = require("src.core.dump")
 local x = pWrapper.find("blockReader")
 
 for index, value in pairs(x.getBlockData()) do
-    print(index .. ": " .. value)
+    print(index .. ": " .. (value or nil))
 end
