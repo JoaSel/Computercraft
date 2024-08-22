08410daadbd6503d0e70c6211b9127117a9ef414
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 15:14:01 +0200

    WIP

--- src/ae2/meMultiBlockRequester.lua
+++ src/ae2/meMultiBlockRequester.lua
@@ -18,6 +18,8 @@ local function getRequiredItems(inTags)
 	for _, item in pairs(inTags) do
 		local existingItem = ret[item.id]
 
+		print(item.id)
+
 		if(existingItem) then
 			existingItem.total = existingItem.total + item["#"]
 			existingItem.needed = existingItem.needed + item["#"]
