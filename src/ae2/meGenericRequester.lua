9bd64a9aa4b75623a8ff46d012a5f8610f55b6bb
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 19:56:02 +0200

    WIP

--- src/ae2/meGenericRequester.lua
+++ src/ae2/meGenericRequester.lua
@@ -4,14 +4,19 @@ package.path = package.path .. ";../core/?.lua"
 
 local file = require("file")
 local net = require("net")
+local mItem = require("moreItem")
 local aeNameUtil = require("libs.aeNameUtil")
 
 local bridge = peripheral.find("meBridge")
 local monitor = peripheral.find("monitor")
 
+local tag = "forge:ingot"
+
 local craftableItems = bridge.listCraftableItems()
 for _, item in pairs(craftableItems) do
-    print(item.name)
+	if(mItem.hasTag(item, tag)) then
+    	print(item.name)
+	end
 end
 
 
