55496d266fd5db9421875852eb499e702c80bc93
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-22 22:02:08 +0200

    WIP

--- src/ae2/meTagRequester.lua
+++ src/ae2/meTagRequester.lua
@@ -16,10 +16,11 @@ local tagInfos =
 		displayName = "Ingots",
 		amount = 256,
 		batch = 16,
-		workers = 1,
+		workers = 2,
 		validationFunc = function(item)
 			return string.match(item.name, "^gtceu:moly")
-		end
+		end,
+		queued = 0
 	}
 }
 
@@ -40,7 +41,7 @@ local function hasTag(item, tag)
 	return false
 end
 
-local function getRequestData()
+local function getDataBlob()
 	local craftableItems = bridge.listCraftableItems()
 
 	local ret = {}
@@ -50,10 +51,12 @@ local function getRequestData()
 				if (not ret[tag]) then
 					ret[tag] = {}
 				end
+
 				table.insert(ret[tag], { 
 					name = item.name,
 					displayName = item.displayName,
-					amount = item.amount
+					amount = item.amount,
+					status = "Queued"
 				})
 			end
 		end
@@ -62,20 +65,87 @@ local function getRequestData()
 	return ret
 end
 
-local tabData = { 30, 10, 10, 10 }
-local function render(requestData)
+local tabData = { 5, 30, 10, 10, 10 }
+local function render(dataBlob)
 	monitor.clear()
 	monitor.setCursorPos(1, 1)
-	mMon.writeTabbedLine(tabData, "Item", "Current", "Wanted", "Group")
 
-	for tag, data in pairs(requestData) do
-		local tagInfo = tagInfos[tag]	
-
-		for k, item in pairs(data) do
-			mMon.writeTabbedLine(tabData, item.displayName, item.amount, tagInfo.amount, tagInfo.displayName)
+	for tag, itemRequests in pairs(dataBlob) do
+		local tagInfo = tagInfos[tag]
+		mMon.writeLine(tagInfo.displayName)
+		
+		for _, itemRequest in pairs(itemRequests) do
+			mMon.writeTabbedLine(tabData, "", itemRequest.displayName, itemRequest.amount, tagInfo.amount, tagInfo.displayName)
 		end
 	end
 end
 
-local requestData = getRequestData()
-render(requestData)
+local function handle(dataBlob)
+	for tag, itemRequests in pairs(dataBlob) do
+		local tagInfo = tagInfos[tag]
+
+		local simultaneousJobs = 0
+		for _, itemRequest in pairs(itemRequests) do
+			if(bridge.isItemCrafting({name = itemRequest.name})) then
+				simultaneousJobs = simultaneousJobs + 1
+			end
+			itemRequest.status = "Crafting"
+		end
+	end
+end
+
+local function updateRequestInfo(itemName, requestInfo, activeGroups, playerOnline)
+	local searchItem = { name = itemName }
+
+	local existingItem = bridge.getItem(searchItem)
+	if (existingItem == nil or existingItem.amount == nil) then
+		existingItem = {
+			amount = 0,
+			name = itemName,
+			displayName = itemName
+		}
+	end
+
+	requestInfo.existingItem = existingItem
+	if (existingItem.amount >= requestInfo.amount) then
+		requestInfo.status = "Ok"
+		return
+	end
+
+	if (requestInfo.offlineOnly and playerOnline) then
+		requestInfo.status = "PlayerOnline"
+		return
+	end
+
+	if (activeGroups.currentJobs >= maxConcurrentJobs) then
+		requestInfo.status = "WorkerBusy"
+		return
+	end
+
+	local isItemCrafing = bridge.isItemCrafting(searchItem)
+	if (isItemCrafing) then
+		requestInfo.status = "Crafting"
+		activeGroups.currentJobs = activeGroups.currentJobs + 1
+		if (requestInfo.workGroup) then
+			activeGroups[requestInfo.workGroup] = true
+		end
+		return
+	end
+
+	local groupRequest = requestInfo.workGroup ~= nil and activeGroups[requestInfo.workGroup]
+	if (groupRequest) then
+		requestInfo.status = "WorkerBusy"
+		return
+	end
+
+	activeGroups.currentJobs = activeGroups.currentJobs + 1
+	if (requestInfo.workGroup) then
+		activeGroups[requestInfo.workGroup] = true
+	end
+
+	requestItem(itemName, requestInfo, existingItem)
+end
+
+local dataBlob = getDataBlob()
+handle(dataBlob)
+render(dataBlob)
