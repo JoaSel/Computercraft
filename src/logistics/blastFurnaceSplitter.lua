341be956bd3ba70864604570c1ecdae8e055cafb
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-23 15:57:08 +0200

    WIP

--- src/logistics/blastFurnaceSplitter.lua
+++ src/logistics/blastFurnaceSplitter.lua
@@ -1,7 +1,7 @@
 --wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua src/logistics/blastFurnaceSplitter.lua
 local evenSplitter = require("libs.evenSplitter")
 
-local input = "entangled:tile_29"
+local input = "entangled:tile_27"
 local destinationType = "entangled:tile"
 
 evenSplitter.create(input, destinationType, true, true)
