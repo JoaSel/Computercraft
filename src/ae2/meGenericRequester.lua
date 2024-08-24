26f48453d69ea880abd9b2936e3e4dfba174647f
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-24 15:43:34 +0200

    WIP

--- src/ae2/meGenericRequester.lua
+++ src/ae2/meGenericRequester.lua
@@ -263,7 +263,7 @@ for _, tagInfo in pairs(tagInfos) do
 	end)
 end
 
-jGui.createClickableText("HomeBtn", "Home", function ()
+jGui.createClickableText("HomeBtn", "×", function ()
 	renderPage = "HOME"
 	render(dataBlob)
 end)
