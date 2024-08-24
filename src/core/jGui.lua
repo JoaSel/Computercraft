b6383816b2661218601ac97882ac38cc51f54de0
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-24 12:15:50 +0200

    WIP

--- src/core/jGui.lua
+++ src/core/jGui.lua
@@ -121,7 +121,7 @@ local function draw(name)
 				for yPos = v.y, v.y + v.height - 1 do
 					_monitor.setBackgroundColor(v.barBackgroundColor)
 					_monitor.setCursorPos(v.x, yPos)
-					_monitor.write(string.rep(" ", v.length))
+					_monitor.write(string.rep(" ", (1 / _textScale) * v.length))
 
 					-- _monitor.setCursorPos(v.x, yPos)
 					-- _monitor.setBackgroundColor(v.barForegroundColor)
