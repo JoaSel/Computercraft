12aca07c7015fff1b3459db686d67ab5261f5ff0
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-24 11:46:04 +0200

    WIP

--- test.lua
+++ test.lua
@@ -3,6 +3,9 @@
 local jGui = require("src.core.jGui")
 local dump = require("src.core.dump")
 
+local monitor = peripheral.find("monitor")
+
+jGui.setMonitor(monitor)
 
 jGui.createSlider("bulkSlider", 100, 2, 25, -2, 3, colors.red, colors.lime, "Percent")
 
