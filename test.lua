8418cf7e51d108112f9eea0772f5526aafb6eeda
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-24 13:56:21 +0200

    WIP

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 --wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua test.lua
 
-package.path = package.path .. ";./core/?.lua"
+package.path = package.path .. ";./src/core/?.lua"
 
 local jGui = require("jGui")
 local dump = require("dump")
