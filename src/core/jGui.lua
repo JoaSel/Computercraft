eb5e7a9dc4c97283b2b6fe5067f92e88c837bad1
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-24 12:35:30 +0200

    WIP

--- src/core/jGui.lua
+++ src/core/jGui.lua
@@ -37,7 +37,7 @@ local function createSlider(name, maxValue, x, y, length, height, barForegroundC
 		infoType = infoTypeLookup[infoType]
 	end
 	if(length < 0) then
-		length = _mWidth + length - x + 1
+		length = _mWidth + length - x + 2
 	end
 
 	print("textScale: " .. _textScale)
