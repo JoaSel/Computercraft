354a7d668602cb200afb65aa76688562b33c52cd
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-25 19:27:50 +0200

    WIP

--- src/ae2/libs/aeNameUtil.lua
+++ src/ae2/libs/aeNameUtil.lua
@@ -1,4 +1,8 @@
 local function toDisplayName(name)
+	if(not name) then
+		return nil
+	end
+	
 	if (string.byte(string.sub(name, 1, 1)) == 167) then
 		return string.sub(string.sub(name, 0, #name - 3), 3)
 	end
