3353419b157faa6dcfbb132f1ff58502c047101c
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-27 17:14:06 +0200

    WIP

--- src/gtceu/machineReader.lua
+++ src/gtceu/machineReader.lua
@@ -10,12 +10,4 @@ local modem = pWrapper.find("modem")
 modem.open(43) -- Open 43 so we can receive replies
 
 -- Send our message
-modem.transmit(15, 43, "Hello, world!")
-
--- And wait for a reply
-local event, side, channel, replyChannel, message, distance
-repeat
-  event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-until channel == 43
-
-print("Received a reply: " .. tostring(message))
+modem.transmit(43, 43, "Hello, world!")
