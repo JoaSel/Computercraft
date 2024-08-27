0220c8f9729c37ab8a98b1872af0e4599a87432e
joar.selving@gmail.com <joar.selving@gmail.com>
2024-08-27 16:12:17 +0200

    WIP

--- src/ae2/meMultiBlockRequester.lua
+++ src/ae2/meMultiBlockRequester.lua
@@ -92,7 +92,7 @@ while (true) do
 
 	local blockData = reader.getBlockData()
 
-	dump.shallow(blockData.storageWrapper.contents)
+	dump.shallow(blockData.storageWrapper.contents.inventory)
 
 	local firstItem = blockData.Items[1]
 	if (not firstItem or firstItem.id ~= "ae2:processing_pattern") then
