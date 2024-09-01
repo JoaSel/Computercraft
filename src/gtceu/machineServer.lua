c16817545afc4dbffdab959d1888762ea47fad99
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-01 10:13:05 +0200

    WIP

--- src/gtceu/machineServer.lua
+++ src/gtceu/machineServer.lua
@@ -53,7 +53,7 @@ local function handleMessages()
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 
     if (channel == sendChannel and message.machineId) then
-      updateMachine(message)
+      --updateMachine(message)
     end
   end
 end
