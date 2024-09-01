4c4fba66be2ea89b12c2b178dc202910be280299
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-01 08:09:25 +0200

    WIP

--- src/core/time.lua
+++ src/core/time.lua
@@ -1,6 +1,6 @@
 local function getTime(locale)
     locale = locale or "utc"
-    return textutils.formatTime(os.time(locale))
+    return textutils.formatTime(os.time(locale), true)
 end
 
 return { getTime = getTime }
