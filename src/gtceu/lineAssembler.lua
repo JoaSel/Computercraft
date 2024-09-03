c4708995e03c1702353178c1d029ece4cdaf4718
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-03 14:18:52 +0200

    WIP

--- src/gtceu/lineAssembler.lua
+++ src/gtceu/lineAssembler.lua
@@ -8,7 +8,7 @@ local gtceuIO = require("libs.gtceuIO")
 
 local machine = pWrapper.find("blockReader")
 
-local input = pWrapper.wrap("entangled:tile_22")
+local input = pWrapper.wrap("entangled:tile_32")
 --local input = pWrapper.wrap("minecraft:shulker_box_0")
 local output = pWrapper.wrap("entangled:tile_21")
 
