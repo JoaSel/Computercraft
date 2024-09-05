874d8e1e0f7fde3314c48305dc79da129a25d37d
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-05 10:38:55 +0200

    WIP

--- src/gtceu/machineServer.lua
+++ src/gtceu/machineServer.lua
@@ -58,12 +58,12 @@ local function updateMachine(machineData)
 
       if(machineData.blockData.recipeLogic.status == "IDLE" and (machineData.hasInputItems or machineData.hasInputFluids)) then
         displayColor = colors.red
-        errorStatus = string.format("Status: ERROR (%s)", machineData.blockData.recipeLogic.status)
+        errorStatus = string.format(" Status: ERROR (%s)", machineData.blockData.recipeLogic.status)
       end
 
       machine.displayFrame:setBackground(displayColor)
-      machine.displayFrame:editItem(1, machineData.machineId)
-      machine.displayFrame:editItem(2, errorStatus or string.format("Status: OK (%s)", machineData.blockData.recipeLogic.status))
+      machine.displayFrame:editItem(1, " " .. machineData.machineId)
+      machine.displayFrame:editItem(2, errorStatus or string.format(" Status: OK (%s)", machineData.blockData.recipeLogic.status))
 end
 
 local function handleMessages()
