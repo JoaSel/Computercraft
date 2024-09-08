4e01934e95eaaa4a3369aacae00ee39f7c6f8c9a
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-08 08:26:16 +0200

    WIP

--- src/gtceu/machineServer.lua
+++ src/gtceu/machineServer.lua
@@ -38,6 +38,7 @@ local flex = main
   :setSize("parent.w", "parent.h - 1")
 
 local function updateMachine(machineData)
+  print("Running update machine on " .. machineData.machineId)
   local exists = machines[machineData.machineId]
       if (exists) then
         machines[machineData.machineId].machineData = machineData
