9419112f0f05eac324be8455aaf365f84af801ab
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-08 07:54:07 +0200

    WIP

--- src/logistics/gregSorter.lua
+++ src/logistics/gregSorter.lua
@@ -40,15 +40,16 @@ local tagsRoutes = {
 
 local miscRoutes = {
 	function(item)
+		if(string.match(item.name, "_salt_ore$")) then
+			return "Storage Input"
+		end
 		if (string.match(item.name, "^gtceu:") and circularSorter.hasTag(item, "forge:ores")) then
 			return "Macerator"
 		end
 		if (string.match(item.name, "^gtceu:") and circularSorter.hasTag(item, "forge:raw_materials")) then
 			return "Macerator"
 		end
-		if(string.match(item.name, "_salt_ore$")) then
-			return "Storage Input"
-		end
+		
 	end
 }
 
