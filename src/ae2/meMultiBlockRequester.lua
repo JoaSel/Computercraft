f58e2f96ec6d7ad55c97fd3e88e7afd26cee431b
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-14 17:38:06 +0200

    WIP

--- src/ae2/meMultiBlockRequester.lua
+++ src/ae2/meMultiBlockRequester.lua
@@ -1,81 +1,117 @@
---wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua src/ae2/meMultiBlockRequester.lua true
+--wget run https://raw.githubusercontent.com/JoaSel/Computercraft/main/install.lua src/ae2/meMultiBlockRequester.lua
 
-package.path = package.path .. ";../core/?.lua" .. ";../../../?.lua"
+package.path = package.path .. ";../core/?.lua"
 
 local pWrapper = require("peripheralWrapper")
-local net = require("net")
 local mTerm = require("moreTerm")
 local dump = require("dump")
-local basalt = require("basalt")
 
 local chest = pWrapper.find("sophisticatedstorage:shulker_box")
 local reader = pWrapper.find("blockReader")
 local bridge = peripheral.find("meBridge")
 
-local translate = {
-    ["minecraft:crafting_shaped"] = "Shaped"
-}
+local function getRequiredItems(inTags)
+    local ret = {}
 
-local maxRecipes = 10
+    for _, item in pairs(inTags) do
+        if (item.id) then
+            local existingItem = ret[item.id]
 
-local function getCrafingRecipe(table, url)
-    local name = url:sub(url:match('^.*()/') + 1, url:len() - 5):gsub("%.", ":")
-
-    table[name] = net.getJson(
-        "https://raw.githubusercontent.com/JoaSel/ComputercraftLibs/main/ATM9/minecraft.crafting_shaped.json")
-end
-
-local function getCrafingRecipes()
-    local ret = {};
-
-    getCrafingRecipe(ret,
-        "https://raw.githubusercontent.com/JoaSel/ComputercraftLibs/main/ATM9/minecraft.crafting_shaped.json")
+            if (existingItem) then
+                existingItem.total = existingItem.total + item["#"]
+                existingItem.needed = existingItem.needed + item["#"]
+            else
+                ret[item.id] = { total = item["#"], needed = item["#"] }
+            end
+        end
+    end
 
     return ret
 end
 
-local craftingRecipes = getCrafingRecipes()
-local main = basalt.createFrame()
-local flex = main
-    :addFlexbox()
-    :setForeground(colors.white)
-    :setBackground(colors.black)
-    :setWrap("wrap")
-    :setPosition(1, 1)
-    :setSize("parent.w", "parent.h")
+local function adjustExistingItems(requiredItems)
+    local existing = chest.list()
 
-local function addCategoryFrame(category, recipes)
-    local categoryName = translate[category] or category
-    local categoryFrame = basalt.createFrame()
-        :hide()
+    for _, existingItem in pairs(requiredItems) do
+        existingItem.needed = existingItem.total
+    end
 
-    categoryFrame:addLabel()
-        :setText("Category: " .. categoryName)
-
-    local recipeList = categoryFrame:addList()
-        :setPosition(1, 3)
-        :setSize("parent.w", "parent.h - 3")
-
-    flex:addButton()
-        :setText(categoryName)
-        :onClick(
-            function()
-                main:hide()
-                categoryFrame:show()
-            end)
-
-    for key, value in pairs(recipes) do
-
-        recipeList:addItem(value.result.item)
-
-        if (key > maxRecipes) then
-            return
+    for _, item in pairs(existing) do
+        local existingItem = requiredItems[item.name]
+        if (existingItem) then
+            existingItem.needed = existingItem.needed - item.count
         end
     end
 end
 
-for category, recipes in pairs(craftingRecipes) do
-    addCategoryFrame(category, recipes)
+local function handleItem(itemName, itemInfo)
+    if (itemInfo.needed <= 0) then
+        return "Exported"
+    end
+
+    local searchTbl = { name = itemName }
+    local inSystemAmount = 0
+    local inSystemItem = bridge.getItem(searchTbl)
+
+    if (inSystemItem and inSystemItem.amount) then
+        inSystemAmount = inSystemItem.amount
+    end
+
+    if (inSystemAmount >= itemInfo.needed) then
+        bridge.exportItem({ name = itemName, count = itemInfo.needed }, "up")
+        return "InSystem"
+    end
+    if (bridge.isItemCrafting(searchTbl)) then
+        return "Crafting"
+    end
+    if (bridge.isItemCraftable(searchTbl)) then
+        bridge.craftItem({ name = itemName, count = itemInfo.needed - inSystemAmount })
+        return "Craftable"
+    else
+        return "Error"
+    end
 end
 
-basalt.autoUpdate()
+local colorTable = {
+    ["Exported"] = colors.green,
+    ["InSystem"] = colors.green,
+    ["Crafting"] = colors.yellow,
+    ["Craftable"] = colors.orange,
+    ["Error"] = colors.red,
+}
+local function render(requiredItems)
+    term.clear()
+    for itemName, itemInfo in pairs(requiredItems) do
+        mTerm.cprint(string.format("%d of %d %s", itemInfo.needed, itemInfo.total, itemName), colorTable
+        [itemInfo.status])
+    end
+end
+
+while (true) do
+    print("Press Enter to run.")
+    local x = io.read()
+
+    local blockData = reader.getBlockData()
+
+    local firstItem = blockData.storageWrapper.contents.inventory.Items[1]
+    if (not firstItem or firstItem.id ~= "ae2:processing_pattern") then
+        print("No pattern found! Put a pattern in first slot.")
+    else
+        local requiredItems = getRequiredItems(firstItem.tag["in"])
+
+        repeat
+            adjustExistingItems(requiredItems)
+
+            local done = true
+            for itemName, itemInfo in pairs(requiredItems) do
+                itemInfo.status = handleItem(itemName, itemInfo)
+                if (itemInfo.status ~= "Exported") then
+                    done = false
+                end
+            end
+
+            render(requiredItems)
+            os.sleep(5)
+        until done
+    end
+end
