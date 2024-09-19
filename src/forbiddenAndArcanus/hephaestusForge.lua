ad9b59e55cc5d8c006ca2b6cb07640a163a2828c
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-19 08:26:58 +0200

    WIP

--- src/forbiddenAndArcanus/hephaestusForge.lua
+++ src/forbiddenAndArcanus/hephaestusForge.lua
@@ -6,7 +6,14 @@ local pWrapper = require("peripheralWrapper")
 local mTerm = require("moreTerm")
 local dump = require("dump")
 
+local input = pWrapper.find("minecraft:chest")
+
 local forge = pWrapper.wrap("modularrouters:modular_router_1")
 local forgeReader = pWrapper.find("blockReader")
+
+local pedestal1 = pWrapper.wrap("modularrouters:modular_router_2")
+local pedestal2 = pWrapper.wrap("modularrouters:modular_router_3")
+local pedestal3 = pWrapper.wrap("modularrouters:modular_router_4")
+local pedestal4 = pWrapper.wrap("modularrouters:modular_router_5")
 
 print(forge.name)
