10c42abc5876c0db103be9531e6c8b21b7576fec
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-20 15:49:03 +0200

    WIP

--- src/gtceu/machineServer.lua
+++ src/gtceu/machineServer.lua
@@ -93,7 +93,9 @@ local function getOrAddMachine(machineData)
     category.machines[machineData.machineName].displayFrame = category.frame
         :addFrame()
         :setSize("parent.w/2 - 1", 2)
-        
+    
+    print(machineData.machineName)
+
     category.machines[machineData.machineName].displayFrame
         :addLabel(machineData.machineName)
         :setTextAlign("center")
