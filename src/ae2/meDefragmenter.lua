9817ef8fc4ed3bd18a86b7cc1eb6697cae536d0d
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-30 16:48:01 +0200

    WIP

--- src/ae2/meDefragmenter.lua
+++ src/ae2/meDefragmenter.lua
@@ -14,20 +14,22 @@ local monitor = pWrapper.find("monitor")
 
 monitor.setTextScale(0.5)
 
-local root = {}
-root.children = {}
+local mainPage = {}
 
-local main = basalt.addMonitor()
+local base = basalt.addMonitor()
     :setForeground(colors.white)
     :setBackground(colors.black)
     :setMonitor(monitor)
 
-root.frame = main
+mainPage.frame = base
     :addFrame()
     :setForeground(colors.white)
-    :setBackground(colors.blue)
+    :setBackground(colors.black)
     :setPosition(1, 2)
     :setSize("parent.w", "parent.h - 1")
 
+mainPage.frame
+	:addLabel("Bulk Item Storage")
 
-	basalt.autoUpdate();
+
+basalt.autoUpdate();
