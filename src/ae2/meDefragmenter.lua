fd059a52ff2d0460cb93ee18618619415cfe8d7a
joar.selving@gmail.com <joar.selving@gmail.com>
2024-09-30 17:00:23 +0200

    WIP

--- src/ae2/meDefragmenter.lua
+++ src/ae2/meDefragmenter.lua
@@ -40,7 +40,7 @@ mainPage.frame
 	:setProgress(50)
 	:setProgressBar(colors.green)
 	:setBackground(colors.red)
-	:setPosition("parent.w * 0.25", 2)
+	:setPosition("parent.w * 0.125", 2)
 	:setSize("parent.w * 0.75", "2")
 
 
