642c0bae50447829e572446e1c23ad15a59312ee
joar.selving@gmail.com <joar.selving@gmail.com>
2024-10-01 14:49:42 +0200

    WIP

--- src/ae2/meFluidDefragmenter.lua
+++ src/ae2/meFluidDefragmenter.lua
@@ -11,7 +11,7 @@ local dump = require("dump")
 local readers = { pWrapper.find("blockReader") }
 
 
-
+local fluidLocations = {}
 
 
 for _, reader in pairs(readers) do
@@ -19,9 +19,21 @@ for _, reader in pairs(readers) do
 
 	for diskNo, inv in pairs(blockData.inv) do
 		if(inv.tag and inv.tag.keys) then
-			for _, value in pairs(inv.tag.keys) do
-				print(diskNo .. " " .. value.id)
+			for _, fluid in pairs(inv.tag.keys) do
+				if(not fluidLocations[fluid.id]) then
+					fluidLocations[fluid.id] = {}
+				end
+				table.add(fluidLocations[fluid.id], { reader = reader.name, diskNo = diskNo})
 			end
+		end
+	end
+end
+
+for fluid, locations in pairs(fluidLocations) do
+	if(#locations > 1) then
+		print(fluid .. " exists: ")
+		for _, location in pairs(locations) do
+			print("\t" .. location.name .. " => " .. location.diskNo)
 		end
 	end
 end
