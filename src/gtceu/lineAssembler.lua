4207ed3ccf18835d5e7616a8c91ef3c5fb0a55cf
joar.selving@gmail.com <joar.selving@gmail.com>
2024-10-03 20:36:06 +0200

    WIP

--- src/gtceu/lineAssembler.lua
+++ src/gtceu/lineAssembler.lua
@@ -8,7 +8,7 @@ local gtceuIO = require("libs.gtceuIO")
 
 local machine = pWrapper.find("blockReader")
 
-local input = pWrapper.wrap("entangled:tile_36")
+local inputs = { pWrapper.wrap("entangled:tile_36"), pWrapper.wrap("entangled:tile_53") }
 --local input = pWrapper.wrap("minecraft:shulker_box_2")
 local output = pWrapper.wrap("entangled:tile_37")
 
@@ -24,39 +24,43 @@ local inputBuses = { gtceuIO.findInputBuses() }
 local inputHatches = { gtceuIO.findInputHatches() }
 
 local outputBuses = { gtceuIO.findOutputBuses() }
-local outputHatches = { gtceuIO.findOutputHatches() }
 
 local function getStatus()
     local data = machine.getBlockData()
     return data.recipeLogic.status
 end
 
-local function importItems()
-    local inputItems = input.list()
+local function getInputItems()
+    for _, input in pairs(inputs) do
+        local list = input.list();
+        if(next(list)) then
+            return input, list
+        end
+    end
+
+    return nil, nil
+end
+
+local function import()
+    local input, inputItems = getInputItems()
+    if(not input or not inputItems) then
+        return
+    end
 
     local busIndex = 1
     for fromSlot, item in pairs(inputItems) do
-
-        if (dataItems[item.name] and item.nbt) then
-            -- print(item.name .. " => " .. dataAccessHatch.name)
-            -- input.pushItems(dataAccessHatch.name, fromSlot)
-        else
-            for i = 1, item.count, 64 do
-                if (busIndex > #inputBuses) then
-                    error("Build a longer assembly line.")
-                end
-                local currentBus = inputBuses[busIndex]
-
-                input.pushItems(currentBus.name, fromSlot, 64)
-                busIndex = busIndex + 1
+        for i = 1, item.count, 64 do
+            if (busIndex > #inputBuses) then
+                error("Build a longer assembly line.")
             end
+            local currentBus = inputBuses[busIndex]
+
+            input.pushItems(currentBus.name, fromSlot, 64)
+            busIndex = busIndex + 1
         end
     end
-end
 
-local function importFluids()
     local inputFluids = input.tanks()
-
     for _, item in pairs(inputFluids) do
         input.pushFluid(inputHatches[1].name)
     end
@@ -80,25 +84,13 @@ local function exportItems(count)
             exportItems(count + 1)
         end
     end
-
-    --local dataAccessItems = dataAccessHatch.list()
-
-    -- for fromSlot, item in pairs(dataAccessItems) do
-    --     print("Exporting " .. item.name)
-    --     dataAccessHatch.pushItems(output.name, fromSlot)
-    -- end
-
-    -- if(#dataAccessHatch.list() > 0) then
-    --     error("Failed to export data items")
-    -- end
 end
 
 while (true) do
     exportItems(0)
 
     if(getStatus() == "IDLE") then
-        importItems()
-        importFluids()
+        import()
     end
 
     os.sleep(1)
